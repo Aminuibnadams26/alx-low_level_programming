1801220bf647bd096378af1c72e0622edebda296
Aminuibnadams26 <aminuibnadams26@gmail.com>
2022-12-12 09:17:11 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 48; Alpha < 58; Alpha++)
+		putchar(Alpha);
+	putchar('\n');
+	return (0);
+}
