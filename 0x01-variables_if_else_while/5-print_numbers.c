e49f61b86851d290ba13495b3c58f3c6e0f74f49
Aminuibnadams26 <aminuibnadams26@gmail.com>
2022-12-12 08:49:42 -0800

    sprints number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 0; Alpha < 10; Alpha++)
+		printf("%d", Alpha);
+	printf("\n");
+	return (0);
+}
