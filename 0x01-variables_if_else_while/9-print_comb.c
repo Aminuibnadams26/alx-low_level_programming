391922aedb028f5396919d05f211eccda4235d96
Aminuibnadams26 <aminuibnadams26@gmail.com>
2022-12-12 10:59:04 -0800

    print reverse

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Alpha;
+
+	for (Alpha = 48; Alpha < 58; Alpha++)
+	{
+		putchar(Alpha);
+		if (Alpha != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
