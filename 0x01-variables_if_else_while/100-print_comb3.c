54adaad4cdb4a4044a842e51884e5ad2d69dd7f8
Aminuibnadams26 <aminuibnadams26@gmail.com>
2022-12-13 02:45:44 -0800

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,28 @@
  */
 int main(void)
 {
-	int num;
+	int num1, num2;
 
-	for (num = 0; num <= 9; num++)
+	num1 = 48;
+	num2 = 48;
+
+	while (num1 < 58)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+		num2 = 48;
+		while (num2 < 58)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 == 56 && num2 == 57)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+			num2++;
+		}
+		num1++;
 	}
 	putchar('\n');
 	return (0);
