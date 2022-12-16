7c1f353ff436095eb31e584a3fb5f25fe33b70a1
Aminuibnadams26 <aminuibnadams26@gmail.com>
2022-12-16 03:41:49 -0800

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 
+int main(void);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
